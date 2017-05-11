05b93dd1e0cddf37fa832f2adc480a5e5043bd92
Pierre Chabiland <pierrot@poyo.local>
2017-05-11 13:09:52 +0200

    improve sync system

--- bin/sync.sh
+++ bin/sync.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 ssh_open_conn(){
-    mkdir -p ~/.ssh/ctl
-    ssh -nNf -o ControlMaster=yes -o ControlPath="$HOME/.ssh/ctl/%L-%r@%h:%p" $1
-    echo "connection ok"
+    port=22
+    host=$(hostname -s)
+    control_path="$HOME/.ssh/ctl/$host-$1:$port"
+    if [[ ! -e $control_path ]]; then
+        echo "try to connect to $1"  
+        mkdir -p ~/.ssh/ctl
+        ssh -nNf -o ControlMaster=yes -o ControlPath="$HOME/.ssh/ctl/%L-%r@%h:%p" $1
+    fi
 }
 
 ssh_close_conn(){
@@ -18,10 +23,28 @@ watch() {
     fswatch -o $1 | while read f; do synchronize $1 $2 $3 && echo "sync done"; done
 }
 
-current_dir="$(cd "$(dirname "$1")"; pwd)/$(basename "$1")"
-echo "watch $current_dir";
-echo "distant directory : $2:~/$3"
-ssh_open_conn $2
-trap "ssh_close_conn $2; exit" SIGINT SIGKILL
-watch $current_dir $2 $3
+command=$1
+echo "execute $command"
+
+current_dir="$(cd "$(dirname "$2")"; pwd)/$(basename "$2")"
+
+if [ "$command" = "watch" ]; then
+    echo "watch $current_dir";
+    echo "distant directory : $3:~/$4"
+    ssh_open_conn $3
+    echo "connection ok"
+    trap "ssh_close_conn $3; exit" SIGINT SIGKILL
+    synchronize $current_dir $3 $4
+    watch $current_dir $3 $4
+elif [ "$command" = "once" ]; then
+    echo "read $current_dir";
+    echo "distant directory : $3:~/$4"
+    ssh_open_conn $3
+    echo "connection ok"
+    synchronize $current_dir $3 $4
+    echo "sync done"
+elif [ "$command" = "close" ]; then
+    ssh_close_conn $2
+fi
+
 
---
+++ src/motors.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+
+exports.list = function motor_list(){
+    
+}
